c56211edcd78a61896a36ef92ffab1537512810a
augustoferreira7 <augusto.enzo@aluno.ifsp.edu.br>
2024-02-21 11:15:45 -0300

    construindo calculadora

--- PrimeiroProjeto2/Program.cs
+++ PrimeiroProjeto2/Program.cs
@@ -17,9 +17,33 @@
             
             Console.WriteLine(  );
             Console.WriteLine("suas informações:");
-            Console.WriteLine("nome: {0}  \nAnos: {1} anos \ncidade: {2} ", nome, idade, cidade); 
+            Console.WriteLine("nome: {0}  \nAnos: {1} anos \ncidade: {2} ", nome, idade, cidade);
 
-          
+
+            // Parte 2 (calculadora)
+
+            Console.WriteLine(  );
+            int numero1, numero2;
+            int soma, sub, div, mult;
+
+            Console.WriteLine("digite dois números a:");
+            numero1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("digite dois números b:");
+            numero2 = int.Parse(Console.ReadLine());
+            Console.WriteLine(  );
+            soma = numero1 + numero2;
+            sub = numero1 - numero2;
+            div = numero1 / numero2;
+            mult = numero1 * numero2;
+
+            Console.WriteLine(  );
+            Console.WriteLine("soma: {0} \nsub: {1} \ndiv: {2} \nmult: {3}", soma, sub, div, mult);
+
+
+
+
+
+
 
         }
     }
